54f211350c57eadf6491a2328271202062d235a8
Gábor Gergely <ggergely@kodfodrasz.net>
2016-01-24 12:49:55 +0100

    Generic programming makes a programmer productive!
     - Type checking done by the compiler! Code is verified at compile time!
     - Data types are reusable!
     - Fun and profit!
    
    Well, not in Java. Java Generics are broken beyond repair. It disturbs everyone, but it doesn't get fixed.
    
    See the AIDS in the diff, to see what it takes to make this simple problem generic!
    Also notice the lot of casts and hacks needed to make this simple task compile and work.
    Also note: it took a lot of trial and error, because of the casts only fail at runtime! So the main advantage os generics is not present in Java. Just use Object! 
    
    This is when I can understand why the Go folks have not implemented Generics:
    If you know cannot (or don't want to) do it properly, you better not even try.
    (By cannot I can imagine for example deadlines, budget problem, or other issues related to other engineering decisions)

--- src/main/java/net/kodfodrasz/matrixspiral/MatrixSpiral.java
+++ src/main/java/net/kodfodrasz/matrixspiral/MatrixSpiral.java
@@ -1,20 +1,26 @@
 package net.kodfodrasz.matrixspiral;
 
+import java.lang.reflect.Array;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class MatrixSpiral {
 
-    public static Integer[] spiral(Integer[][] matrix) {
-        return spiral(matrix, Stream.empty()).toArray(Integer[]::new);
+    public static <T> T[] spiral(T[][] matrix) {
+        IntFunction arrayAllocator =  i -> allocateGenericArray((Class<T[]>)matrix.getClass().getComponentType(), i);
+
+        
+        return (T[])spiral(matrix, Stream.empty()).toArray(arrayAllocator);
     }
 
-    private static Stream<Integer> spiral(Integer[][] matrix, Stream<Integer> head) {
-        Integer[][] remainder = submatrix(matrix);
-        Stream<Integer> visited = Stream.concat(head, Stream.of(edgeItemsClockwise(matrix)));
+    private static <T> Stream<T> spiral(T[][] matrix, Stream<T> head) {
+        T[][] remainder = submatrix(matrix);
+        Stream<T> visited = Stream.concat(head, Stream.of(edgeItemsClockwise(matrix)));
 
         if (remainder == null) {
             return visited;
@@ -23,28 +29,36 @@ public class MatrixSpiral {
         }
     }
 
-    public static Integer[] edgeItemsClockwise(Integer[][] matrix) {
-        return concatStreams( // this is AIDS 
+    public static <T> T[] edgeItemsClockwise(T[][] matrix) {
+        IntFunction outerArrayAllocator = i -> allocateGenericArray((Class<T[][]>)matrix.getClass(), i);
+        IntFunction innerArrayAllocator = i -> allocateGenericArray((Class<T[]>)matrix.getClass().getComponentType(), i);
+        
+        Supplier emptyArray = () -> (T[])innerArrayAllocator.apply(0);
+        
+        return (T[]) concatStreams( // this is AIDS 
                 // first row
-                Stream.of(Stream.of(matrix).findFirst().orElse(new Integer[0])),
+                Stream.of(Stream.of(matrix).findFirst().orElseGet(emptyArray)),
                 // internal row -> { row[last] } 
                 Stream.of(matrix).skip(1).limit(Math.max(0, matrix.length - 2))
                 .flatMap(arr -> reverseStreamOfArray(arr).limit(1)),
                 // last row, reversed
-                reverseStreamOfArray(Stream.of(matrix).skip(Math.max(1, matrix.length - 1)).findFirst().orElse(new Integer[0])),
+                reverseStreamOfArray(Stream.of(matrix).skip(Math.max(1, matrix.length - 1)).findFirst().orElseGet(emptyArray)),
                 // reverse order of internal rows, then: internal row -> { row[first] if row has nultiple elements } 
-                reverseStreamOfArray(Stream.of(matrix).skip(1).limit(Math.max(0, matrix.length - 2)).toArray(Integer[][]::new))
-                .flatMap(arr -> reverseStreamOfArray(arr).skip(Math.max(1, arr.length - 1)).limit(1))
-        ).toArray(Integer[]::new);
+                reverseStreamOfArray(Stream.of(matrix).skip(1).limit(Math.max(0, matrix.length - 2)).toArray(outerArrayAllocator))
+                .flatMap(arr -> reverseStreamOfArray((T[])arr).skip(Math.max(1, ((T[])arr).length - 1)).limit(1))
+        ).toArray(innerArrayAllocator);
     }
 
-    public static Integer[][] submatrix(Integer[][] matrix) {
-        Integer[][] result = Stream.of(matrix)
+    public static <T> T[][] submatrix(T[][] matrix) {
+        IntFunction outerArrayAllocator = i -> allocateGenericArray((Class<T[][]>)matrix.getClass(), i);
+        IntFunction innerArrayAllocator = i -> allocateGenericArray((Class<T[]>)matrix.getClass().getComponentType(), i);
+        
+        T[][] result = (T[][])Stream.of(matrix)
                 .skip(1)
                 .limit(Math.max(0, matrix.length - 2))
-                .map(arr -> Stream.of(arr).skip(1).limit(Math.max(0, arr.length - 2)).toArray(Integer[]::new))
-                .filter(arr -> arr.length != 0)
-                .toArray(Integer[][]::new);
+                .map(arr -> (T[])Stream.of(arr).skip(1).limit(Math.max(0, arr.length - 2)).toArray(innerArrayAllocator))
+                .filter(arr -> ((T[])arr).length != 0)
+                .toArray(outerArrayAllocator);
         return result.length > 0 ? result : null;
     }
 
@@ -71,4 +85,9 @@ public class MatrixSpiral {
         return Stream.of(streams).reduce(Stream.empty(), (a, b) -> Stream.concat(a, b));
     }
 
+    public static <E> E[] allocateGenericArray(Class<E[]> clazz, int length) {
+        // AIDS intensifies!
+        E[] a = (E[])clazz.cast(Array.newInstance(clazz.getComponentType(), length));  
+        return a;
+    } 
 }
